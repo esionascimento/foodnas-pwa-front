7e49e7cf1aac415c9a9a9deba18cffec943e744b
esionascimento <esionascimento@protonmail.com>
2022-07-11 12:32:58 -0400

    create: page restaurantes

--- src/pages/restaurantes/index.tsx
+++ src/pages/restaurantes/index.tsx
@@ -1,6 +1,16 @@
+import { Header } from "src/components/Header";
+import LojasComponent from 'src/components/Lojas';
+
 const Restaurantes = () => {
   return (
     <>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+        <LojasComponent />
+        {/* <main className="mt-16 px-4 md:mt-20 md:px-16 py-4 w-full">
+          <Container />
+        </main> */}
+      </div>
     </>
   )
 }
