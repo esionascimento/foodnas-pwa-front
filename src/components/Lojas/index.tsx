bfc362baac37c2762c92a13f6b7c02b78052a573
esionascimento <esionascimento@protonmail.com>
2022-07-11 12:30:37 -0400

    create: component Lojas

---
+++ src/components/Lojas/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const LojasComponent = () => {
+  return (
+    <>
+      <div>
+        <p>Lojas</p>
+        {/* <ul>
+          {data?.lessons.map(lesson => {
+            return <li key={lesson.id}>{lesson.title}</li>
+          })}
+        </ul> */}
+      </div>
+    </>
+  )
+}
+
+export default LojasComponent;
