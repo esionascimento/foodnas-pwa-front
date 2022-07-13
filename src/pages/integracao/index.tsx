c286415562d842c2954333e5a2931665fde78cbe
esionascimento <esionascimento@protonmail.com>
2022-07-13 10:53:03 -0400

    create: page Integracao

---
+++ src/pages/integracao/index.tsx
@@ -0,0 +1,18 @@
+import { Header } from "src/components/Header";
+// import LojasComponent from 'src/components/Lojas';
+import Scanner from 'src/components/Scanner';
+
+const Integracao = () => {
+  return (
+    <>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+        <main className="mt-16 px-4 md:mt-20 md:px-16 py-4 w-full">
+          <Scanner />
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default Integracao;
