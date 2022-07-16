d52535d157c1f3ae49e29d3d4ccadef677b01a36
esionascimento <esionascimento@protonmail.com>
2022-07-16 17:27:12 -0400

    create: config dashboard

---
+++ src/config/dashboard/dashboard.ts
@@ -0,0 +1,35 @@
+export const buttonNewOrder = {
+  text: "Novo Pedido",
+  href: "/restaurantes",
+  config: {
+    maxLength: 8,
+    step: 1,
+    background: "bg-orange-400",
+    text: "",
+    size: "text-lg"
+  }
+}
+
+export const buttonRegisterSales = {
+  text: "Registrar Venda",
+  href: "/register-sale",
+  config: {
+    maxLength: 8,
+    step: 1,
+    background: "bg-orange-400",
+    text: "",
+    size: "text-lg"
+  }
+}
+
+export const buttonAddStock = {
+  text: "Adicionar Estoque",
+  href: "/add-stock",
+  config: {
+    maxLength: 8,
+    step: 1,
+    background: "bg-orange-400",
+    text: "",
+    size: "text-lg"
+  }
+}
