4ebf7711fd4b58e7387f223e516c3dd07fb5df10
esionascimento <esionascimento@protonmail.com>
2022-07-16 18:19:34 -0400

    create: page adicionar-stock

---
+++ src/pages/adicionar-stock/index.tsx
@@ -0,0 +1,16 @@
+import { Header } from "src/components/Header";
+import Container from 'src/components/Container/Dashboard';
+
+const Dashboard = () => {
+  return (
+    <div className="w-screen h-auto flex flex-col bg-primary">
+      <Header />
+
+      <main className="mt-16 px-4 md:mt-20 md:px-16 py-4 w-full">
+        <Container />
+      </main>
+    </div>
+  )
+}
+
+export default Dashboard
