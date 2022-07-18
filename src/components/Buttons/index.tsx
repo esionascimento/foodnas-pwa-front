c2d5f40af248b4bdbfcbe578898902be3caecf3b
esionascimento <esionascimento@protonmail.com>
2022-07-18 11:59:11 -0400

    refactor: component Buttons css legible

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -3,11 +3,13 @@ import { IDashboardButton } from 'src/interface/config/dashboard';
 import Link from "next/link";
 
 const ButtonComponent:FC<IDashboardButton> = (props) => {
+  const className = ` w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 ${props.config.background} ${props.config.size}`;
+
   return (
     <Link href={props.href}>
       <button
         type="button"
-        className={`bg-gradient-to-br w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 ${props.config.background} ${props.config.size}`}
+        className={className}
       >
         {props.text}
       </button>
