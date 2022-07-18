53ca7c50ffde847a28274947490ef31afc37f4aa
esionascimento <esionascimento@protonmail.com>
2022-07-18 19:02:49 -0400

    remove: code comments

--- src/components/Container/AddStock/index.tsx
+++ src/components/Container/AddStock/index.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { MdAccountCircle, MdDeliveryDining } from "react-icons/md";
 import ButtonComponent from 'src/components/Buttons';
 import { buttonRegisterSales, buttonAddStock } from 'src/config/dashboard/dashboard';
-import { inputTwoPart, inputThreePart } from "src/config/add-stock";
+import { inputTwoPart, inputThreePart, inputFourPart } from "src/config/add-stock";
 import { useForm } from "react-hook-form";
 import Inputs from 'src/components/Inputs';
+import Scanner from 'src/components/Scanner';
 
 const AddStock = () => {
   const { control, handleSubmit, register, setValue } = useForm();
+  const [valueBarcode, setValueBarcode] = useState();
 
   return (
     <>
@@ -24,9 +27,10 @@ const AddStock = () => {
                 <input
                   type="number"
                   id="barcode"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[40%] p-1 mr-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-1 mr-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder=""
                   required
+                  value={valueBarcode}
                 />
                 <button
                   type="button"
@@ -43,12 +47,13 @@ const AddStock = () => {
               >
                 Digitalizar Código   
               </label>
-              <button
+              <Scanner setValueBarcode={setValueBarcode}/>
+              {/* <button
                 type="button"
-                className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-1.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm p-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 Ler
-              </button>
+              </button> */}
             </div>
           </div>
           <div className="py-2 flex justify-between items-center">
@@ -61,6 +66,15 @@ const AddStock = () => {
           <div className="py-2 flex justify-between items-center">
             <div className="grid grid-cols-3 md:grid-cols-2  divide-black">
               {inputThreePart.map((data) => {
+                return (
+                  <Inputs key={data.id} {...data} control={control} />
+                )
+              })}
+            </div>
+          </div>
+          <div className="py-2 flex justify-between items-center">
+            <div className="grid grid-cols-3 md:grid-cols-2  divide-black">
+              {inputFourPart.map((data) => {
                 return (
                   <Inputs key={data.id} {...data} control={control} />
                 )
