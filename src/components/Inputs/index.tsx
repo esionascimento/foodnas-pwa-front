528c2c22ad8d825320f8f8df69fe2b9a8dde420d
esionascimento <esionascimento@protonmail.com>
2022-07-23 17:23:25 -0400

    refactor: component Inputs - className dynamic

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -2,6 +2,9 @@ import { IComponentPropsInputs } from "src/interface/add-stock";
 import { Controller } from 'react-hook-form';
 
 const Inputs = (props: IComponentPropsInputs | any) => {
+
+  const className = `bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 ${props.disabled ? 'dark:bg-gray-700' : ''} dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500`
+
   return (
     <div className="flex flex-col justify-between self-stretch m-1	">
       <label
@@ -21,19 +24,19 @@ const Inputs = (props: IComponentPropsInputs | any) => {
                 type={props.config.type}
                 id={props.id}
                 disabled={props.disabled}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className={className}
                 placeholder=""
                 required
               />
             ) : (
               <select
                 id="countries"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className={className}
               >
                 {props.option.map((data: any) => {
                   return (
                     <>
-                      <option selected value={data.id}>{data.text}</option>
+                      <option className='' selected value={data.id}>{data.text}</option>
                     </>
                   )
                 })}
@@ -47,19 +50,3 @@ const Inputs = (props: IComponentPropsInputs | any) => {
 }
 
 export default Inputs;
-
-// <div className="m-1">
-//   <label
-//     htmlFor={props.id}
-//     className="text-sm font-medium text-black"
-//     >
-//     {props.text}
-//   </label>
-//   <input
-//     type={props.config.type}
-//     id={props.id}
-//     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//     placeholder=""
-//     required
-//     />
-// </div>
