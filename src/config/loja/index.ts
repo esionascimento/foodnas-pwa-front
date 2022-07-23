5c2760f2e4f2a24c1a148382b06cc0082a39ae16
esionascimento <esionascimento@protonmail.com>
2022-07-23 17:22:22 -0400

    create: config loja

---
+++ src/config/loja/index.ts
@@ -0,0 +1,154 @@
+import { IComponentPropsInputs } from "src/interface/inputs";
+
+export const inputTwoPart: IComponentPropsInputs[] = [
+  {
+    id: 'name',
+    text: "Nome",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'text',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'typePacking',
+    text: "Caixa | UND",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'select',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    },
+    option: [
+      {id: "caixa", text: "Caixa"},
+      {id: "separado", text: "Separado"},
+    ],
+  },
+  // {
+  //   id: 'type',
+  //   text: "Tipo",
+  //   disabled: false,
+  //   visible: false,
+  //   config: {
+  //     type: 'text',
+  //     maxLength: 8,
+  //     step: 1,
+  //     background: "bg-bgButtonOrange",
+  //     text: "",
+  //     size: "text-md"
+  //   }
+  // },
+]
+
+export const inputThreePart: IComponentPropsInputs[] = [
+  {
+    id: 'ml',
+    text: "ML",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'text',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'qtdBoxes',
+    text: "QTD em caixa",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'number',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'addQtd',
+    text: "Adicionar QTD",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'number',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'value',
+    text: "Valor Unitário",
+    disabled: false,
+    visible: true,
+    config: {
+      type: 'text',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+]
+
+export const inputFourPart: IComponentPropsInputs[] = [
+  {
+    id: 'qtdSave',
+    text: "Nome",
+    disabled: true,
+    visible: true,
+    config: {
+      type: 'number',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'valueUnitary',
+    text: "Tipo",
+    disabled: true,
+    visible: true,
+    config: {
+      type: 'text',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+  {
+    id: 'amount',
+    text: "Código de barra",
+    disabled: true,
+    visible: true,
+    config: {
+      type: 'text',
+      maxLength: 8,
+      step: 1,
+      background: "bg-bgButtonOrange",
+      text: "",
+      size: "text-md"
+    }
+  },
+]
