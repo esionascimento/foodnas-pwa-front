eaf280c4dd4c42966009fea0295af02c6f19688e
esionascimento <esionascimento@protonmail.com>
2022-07-23 16:28:37 -0400

    refactor: change href header menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -134,7 +134,7 @@ const HeaderComponent = () => {
                         </li>
                       )
                       : (
-                        <Link href="/integracao">
+                        <Link href={data.href}>
                           <li
                             className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                             >
