08c7cacd8d98c72f1b4c57d9e728a1f6c93a2aeb
esionascimento <esionascimento@protonmail.com>
2022-07-24 01:07:40 -0400

    refactor: condition input visible

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -6,46 +6,52 @@ const Inputs = (props: IComponentPropsInputs | any) => {
   const className = `bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 ${props.disabled ? 'dark:bg-gray-700' : ''} dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500`
 
   return (
-    <div className="flex flex-col justify-between self-stretch m-1	">
-      <label
-        htmlFor={props.id}
-        className="text-sm font-medium text-black"
-        >
-        {props.text}
-      </label>
-      <Controller
-        key={`${props.id}`}
-        control={props.control}
-        name={props.id}
-        render={({ field, fieldState: { error } }) => {
-          return (
-            props.config.type === 'text' || props.config.type === 'number' ? (
-              <input
-                type={props.config.type}
-                id={props.id}
-                disabled={props.disabled}
-                className={className}
-                placeholder=""
-                required
-              />
-            ) : (
-              <select
-                id="countries"
-                className={className}
-              >
-                {props.option.map((data: any) => {
-                  return (
-                    <>
-                      <option className='' selected value={data.id}>{data.text}</option>
-                    </>
-                  )
-                })}
-              </select>
-            )
-          )
-        }}
-      />
-    </div>
+    <>
+      {props.visible ? (
+        <div className="flex flex-col justify-between self-stretch m-1">
+          <label
+            htmlFor={props.id}
+            className="text-sm font-medium text-black"
+            >
+            {props.text}
+          </label>
+          <Controller
+            key={`${props.id}-${props.title}`}
+            control={props.control}
+            defaultValue={""}
+            name={props.id}
+            render={({ field, fieldState: { error } }) => {
+              return (
+                props.config.type === 'text' || props.config.type === 'number' ? (
+                  <input
+                    {...field}
+                    type={props.config.type}
+                    id={props.id}
+                    disabled={props.disabled}
+                    className={className}
+                    placeholder=""
+                    required
+                  />
+                ) : (
+                  <select
+                    {...field}
+                    id="countries"
+                    className={className}
+                  >
+                    {props.option.map((data: any, index: number) => {
+                      return (
+                        <option key={`${data.id}-${index}-inputs`} className='' value={data.id}>{data.text}</option>
+                      )
+                    })}
+                  </select>
+                )
+              )
+            }}
+          />
+        </div>
+      ) : ( null )
+      }
+    </>
   )
 }
 
