4a2e679fb51632ebf6742dc6eac2919216501b16
esionascimento <esionascimento@protonmail.com>
2022-08-13 03:06:25 -0400

    create: config dos menus hamburguer do Header

--- src/config/header/index.ts
+++ src/config/header/index.ts
@@ -5,3 +5,22 @@ export const configMenuLi = [
   {text: 'Serviços', href: '/servicos'},
   {text: 'Sair', href: '/dashboard'}
 ]
+
+export const configHeaderMenuHamburguer = [
+  {
+    text: 'Loja',
+    href: '/loja',
+    config: {
+      type: 'number',
+      maxLength: 8,
+      hoverText: 'hover:text-gray-100',
+      background: 'bg-bgButtonOrange',
+      colorText: 'text-blue-400',
+      size: 'text-md',
+    }
+  },
+  {text: 'Integracao', href: '/integracao'},
+  {text: 'Sobre', href: '/sobre'},
+  {text: 'Serviços', href: '/servicos'},
+  {text: 'Sair', href: '/dashboard'}
+]
